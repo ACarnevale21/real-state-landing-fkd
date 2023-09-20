bf05de95f49053a424ddf4436adae507a64eb0a4
AgustinCarnevaleEscritorio <carnevaleagustinj@gmail.com>
2023-09-20 02:36:02 -0300

    changed contact form to emailjs deleted chakra, responsive herosection

--- app/components/Contact/ContactForm/index.tsx
+++ app/components/Contact/ContactForm/index.tsx
@@ -1,15 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Image from "next/image";
-import {
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-  Textarea,
-  Container,
-  Button,
-} from "@chakra-ui/react";
+
+import emailjs from "@emailjs/browser";
 import {
   ContactFormValues,
   InitialTouchedValuesInterface,
@@ -37,6 +30,7 @@ const initTouched: InitialTouchedValuesInterface = {
 const ContactForm = () => {
   const [state, setState] = useState(initState);
   const [touched, setTouched] = useState(initTouched);
+  const form = useRef<HTMLFormElement | null>(null);
 
   const { values, isLoading } = state;
 
@@ -57,11 +51,21 @@ const ContactForm = () => {
     }));
   };
 
-  const onSubmit = async () => {
-    setState((prev) => ({
-      ...prev,
-      isLoading: true,
-    }));
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm("SERVICE_ID", "TEMPLATE_ID", form.current, "PUBLIC_KEY")
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
   };
 
   return (
@@ -85,175 +89,128 @@ const ContactForm = () => {
         </div>
 
         <div className="flex justify-center content-center items-center md:flex-col">
-          <Container className="flex flex-wrap -m-2">
-            <div className="lg:flex flex-row p-2 w-full md:flex-row">
-              <FormControl
-                className="relative"
-                isRequired
-                isInvalid={touched.name && !values.name}
-              >
-                <FormLabel
-                  htmlFor="name"
-                  className="leading-7 text-sm text-gray-800 font-semibold"
-                >
-                  Name
-                </FormLabel>
-                <Input
-                  type="text"
-                  id="name"
-                  errorBorderColor="red.400"
-                  value={values.name}
-                  onChange={handleChange}
-                  onBlur={onBlur}
-                  name="name"
-                  placeholder="Full name"
-                  className="w-full bg-gray-200 rounded border border-gray-300 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-                <FormErrorMessage>Required</FormErrorMessage>
-              </FormControl>
-              <FormControl
-                className="lg:pl-2 relative  md:pl-0 sm:pl-0"
-                isRequired
-                isInvalid={touched.lastName && !values.lastName}
-              >
-                <FormLabel
-                  htmlFor="name"
-                  className="leading-7 text-sm text-gray-800 font-semibold"
-                >
-                  Last Name
-                </FormLabel>
-                <Input
-                  type="text"
-                  id="lastName"
-                  errorBorderColor="red.400"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  onBlur={onBlur}
-                  name="lastName"
-                  placeholder="Last name"
-                  className="w-full bg-gray-200 rounded border border-gray-300 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-                <FormErrorMessage>Required</FormErrorMessage>
-              </FormControl>
-            </div>
-            <div className="p-2 w-full">
-              <FormControl
-                className="relative"
-                isRequired
-                isInvalid={touched.email && !values.email}
-              >
-                <FormLabel
+          <div className="flex flex-wrap -m-2">
+            <form className="relative" ref={form} onSubmit={sendEmail}>
+              <div className="flex-row p-2 w-full md:flex-row lg:flex">
+                <div className="md:mr-2">
+                  <label
+                    htmlFor="name"
+                    className="leading-7 text-sm text-gray-800 font-semibold"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={onBlur}
+                    name="name"
+                    placeholder="Full name"
+                    className="w-full bg-gray-200 rounded border border-gray-300 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="lastName"
+                    className="leading-7 text-sm text-gray-800 font-semibold"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={onBlur}
+                    name="lastName"
+                    placeholder="Last name"
+                    className="w-full bg-gray-200 rounded border border-gray-300 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <label
                   htmlFor="email"
                   className="leading-7 text-sm text-gray-800 font-semibold"
                 >
                   Email
-                </FormLabel>
-                <Input
+                </label>
+                <input
                   type="email"
                   id="email"
                   name="email"
-                  errorBorderColor="red.400"
                   value={values.email}
                   onBlur={onBlur}
                   onChange={handleChange}
                   placeholder="email@address.com"
                   className="w-full bg-gray-200 rounded border border-gray-300 focus:border-yellow-500 focus:bg-yellow-6-0 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
-                <FormErrorMessage>Required</FormErrorMessage>
-              </FormControl>
-            </div>
-            <FormControl
-              className="p-2 w-full"
-              isRequired
-              isInvalid={touched.phone && !values.phone}
-            >
-              <FormLabel
-                htmlFor="tel"
-                className="leading-7 text-sm text-gray-800 font-semibold"
-              >
-                Phone Number
-              </FormLabel>
-              <Input
-                type="tel"
-                name="phone"
-                id="phone"
-                errorBorderColor="red.400"
-                value={values.phone}
-                onChange={handleChange}
-                onBlur={onBlur}
-                placeholder="+1 (xxx) xxx-xx-xx"
-                className="w-full bg-gray-200 rounded border border-gray-300 focus:border-yellow-500 focus:bg-yellow-6-0 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-              <FormErrorMessage>Required</FormErrorMessage>
-            </FormControl>
-            <FormControl
-              className="p-2 w-full"
-              isRequired
-              isInvalid={touched.address && !values.address}
-            >
-              <FormLabel
-                htmlFor="address"
-                className="leading-7 text-sm text-gray-800 font-semibold"
-              >
-                Enter Property Address
-              </FormLabel>
-              <Input
-                type="text"
-                name="address"
-                id="address"
-                errorBorderColor="red.400"
-                value={values.address}
-                onChange={handleChange}
-                onBlur={onBlur}
-                placeholder="49 Smith St.
-                Saint Cloud, MN 56301"
-                className="w-full bg-gray-200 rounded border border-gray-300 focus:border-yellow-500 focus:bg-yellow-6-0 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-              <FormErrorMessage>Required</FormErrorMessage>
-            </FormControl>
-            <div className="p-2 w-full">
-              <FormControl
-                className="relative"
-                isRequired
-                isInvalid={touched.message && !values.message}
-              >
-                <FormLabel
+              </div>
+              <div className="p-2 w-full">
+                <label
+                  htmlFor="phone"
+                  className="leading-7 text-sm text-gray-800 font-semibold"
+                >
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  value={values.phone}
+                  onChange={handleChange}
+                  onBlur={onBlur}
+                  placeholder="+1 (xxx) xxx-xx-xx"
+                  className="w-full bg-gray-200 rounded border border-gray-300 focus:border-yellow-500 focus:bg-yellow-6-0 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div className="p-2 w-full">
+                <label
+                  htmlFor="address"
+                  className="leading-7 text-sm text-gray-800 font-semibold"
+                >
+                  Enter Property Address
+                </label>
+                <input
+                  type="text"
+                  name="address"
+                  id="address"
+                  value={values.address}
+                  onChange={handleChange}
+                  onBlur={onBlur}
+                  placeholder="49 Smith St. Saint Cloud, MN 56301"
+                  className="w-full bg-gray-200 rounded border border-gray-300 focus:border-yellow-500 focus:bg-yellow-6-0 focus:ring-2 focus:ring-yellow-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div className="p-2 w-full">
+                <label
                   htmlFor="message"
                   className="leading-7 text-sm text-gray-800 font-semibold"
                 >
                   What&apos;s your situation?
-                </FormLabel>
-                <Textarea
+                </label>
+                <textarea
                   id="message"
                   name="message"
                   rows={4}
-                  errorBorderColor="red.400"
                   value={values.message}
                   onChange={handleChange}
                   onBlur={onBlur}
                   placeholder="Tell us about your situation.."
                   className="w-full bg-gray-200 rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-600 h-32 text-base outline-none text-gray-900 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                 />
-                <FormErrorMessage>Required</FormErrorMessage>
-              </FormControl>
-            </div>
-            <div className="p-2 w-full">
-              <Button
-                disabled={
-                  !values.name ||
-                  !values.email ||
-                  !values.lastName ||
-                  !values.phone ||
-                  !values.message
-                }
-                onClick={onSubmit}
-                isLoading={isLoading}
-                className="flex mx-auto transition ease-in-out delay-0 text-gray-900  bg-yellow-400 border-0 py-2 px-8 focus:outline-none hover:-translate-y-1 hover:scale-110 hover:duration-300 rounded text-lg"
-              >
-                Send
-              </Button>
-            </div>
-          </Container>
+              </div>
+              <div className="p-2 w-full">
+                <button
+                  type="submit"
+                  className="flex mx-auto transition ease-in-out delay-0 text-gray-900  bg-yellow-400 border-0 py-2 px-8 focus:outline-none hover:-translate-y-1 hover:scale-110 hover:duration-300 rounded text-lg"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
